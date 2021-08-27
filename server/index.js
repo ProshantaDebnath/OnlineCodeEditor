5574fe3f5834ef17f798ea6482ae42f20ed6c4db
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2021-08-28 01:03:13 +0530

    host backend part in locally

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Library Imports
+const express = require("express");
+const bodyParser = require("body-parser")
+
+// Project Imports
+const routes = require("./routes")
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use('/', routes) 
+
+app.listen(PORT, ()=>{
+console.log(`Server started at port ${PORT}`)
+})
